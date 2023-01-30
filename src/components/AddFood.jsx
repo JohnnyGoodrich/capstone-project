2b472ac5cb131cd202d6622f0c70ca8926cb9df0
JohnnyGoodrich <jbgoodrich26@gmail.com>
2023-01-30 17:24:59 -0500

    post route working in app

--- src/components/AddFood.jsx
+++ src/components/AddFood.jsx
@@ -1,19 +1,174 @@
 import { useParams, useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
-function AddFood(){
+function AddFood() {
     const params = useParams()
-    const {id} = params
+    const { id } = params
     const [food, setFood] = useState(null)
+    const [editForm, setEditForm] = useState({
+        name: "",
+        calories: "",
+        protein: "",
+        carbs: "",
+        fat: "",
+        image: "",
+    })
 
     const URL = "http://localhost:4000/food"
     const URL2 = `http://localhost:4000/food/${id}`
 
+    // handlechange and handlesubmit
+    const handleChange = (e) => {
+        const userInput = { ...editForm }
+        userInput[e.target.name] = e.target.value
+        setEditForm(userInput)
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const currentState = { ...editForm }
+
+        try {
+            const requestOptions = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    // Authorization: `Bearer ${token}`
+                },
+                body: JSON.stringify(currentState)
+
+            }
+            const response = await fetch(URL, requestOptions)
+            const createdFood = await response.json()
+            setFood([...food, createdFood])
+            setEditForm([{
+                name: "",
+                Calories: "",
+                Protein: "",
+                Carbs: "",
+                Fat: "",
+                image: "",
+            }])
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    // end handle functions
 
 
-    return(
-    <h1>Add Food Page</h1>
+    const loaded = () => (
+        <div>
+            <form className='rating-form' onSubmit={handleSubmit}>
+                <h2 className='section-header'>Create a new Review</h2>
+                <div className='create-review'>
+                    <label className='comment-label' htmlFor='title'>
+                        <div>Name</div>
+                        <input
+                            type="text"
+                            className="name"
+                            name="name"
+                            placeholder="name"
+                            autoComplete='off'
+                            value={editForm.name}
+                            onChange={handleChange}
+                        />
+                    </label>
+                    <div>Calories</div>
+                    <label htmlFor='title'>
+                        <input
+                            type="number"
+                            className="calories"
+                            name="calories"
+                            // placeholder="1-100"
+                            min="0"
+                            max="100"
+                            autoComplete='off'
+                            value={editForm.calories}
+                            onChange={handleChange}
+                        />
+                    </label>
+                    <label className='comment-label' htmlFor=''>
+                        <div>Protein</div>
+                        <input
+                            type="number"
+                            className="protein"
+                            name="protein"
+                            // placeholder="1-100"
+                            min="0"
+                            max="100"
+                            autoComplete='off'
+                            value={editForm.protein}
+                            onChange={handleChange}
+                        />
+                    </label>
+                    <label className='comment-label' htmlFor='title'>
+                        <div>Carbs</div>
+                        <input
+                            type="number"
+                            className="carbs"
+                            name="carbs"
+                            // placeholder="1-100"
+                            min="0"
+                            max="100"
+                            autoComplete='off'
+                            value={editForm.carbs}
+                            onChange={handleChange}
+                        />
+                    </label>
+                    <label className='comment-label' htmlFor='title'>
+                        <div>Fat</div>
+                        <input
+                            type="number"
+                            className="fat"
+                            name="fat"
+                            // placeholder="1-100"
+                            min="0"
+                            max="100"
+                            autoComplete='off'
+                            value={editForm.fat}
+                            onChange={handleChange}
+                        />
+                    </label>
+                    <label className='comment-label' htmlFor='title'>
+                        <div>Image</div>
+                        <input
+                            type="text"
+                            className="image"
+                            name="image"
+                            // placeholder="1-100"
+                            min="0"
+                            max="100"
+                            autoComplete='off'
+                            value={editForm.image}
+                            onChange={handleChange}
+                        />
+                    </label>
+
+                    <br />
+                    <div className='button'>
+                        <button type="submit" value="Post Review">Post Review</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     )
+
+
+    const loading = () => (
+        <>
+            <h1>
+                Loading...
+            </h1>
+        </>
+    );
+    return (
+        <div>
+            {/* {food ? loaded() : loading()} */}
+            {loaded()}
+
+        </div>
+    )
+
 }
 
 export default AddFood
